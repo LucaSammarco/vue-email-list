bf133d1cf3ee3dbbdf2fd6e0da01b2280b2d0b79
Il tuo nome <la_tua_email@example.com>
2024-06-05 16:33:22 +0200

    Set up

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+
+const { createApp } = Vue;
+
+
+
+const DateTime = luxon.DateTime;
+const now = DateTime.now();
+
+
+
+
+
+createApp({
+    data() {
+        return {
+
+        };
+
+    },
+
+    methods: {
+
+     
+
+
+    },
+
+   
+    
+}).mount("#app");
+
